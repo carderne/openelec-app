1e00d7fcd1ab0b8337126b80a411f5753809ac0b
Chris Arderne <chris@rdrn.me>
2018-12-01 10:37:11 +0100

    Add JS hashes for caching and split JS into main, runtime and vendors

--- webpack.config.js
+++ webpack.config.js
@@ -1,11 +1,25 @@
 const path = require('path');
+const webpack = require('webpack');
+const CleanWebpackPlugin = require('clean-webpack-plugin');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
   entry: './src/index.js',
+  plugins: [
+    new CleanWebpackPlugin(['dist']),
+    new HtmlWebpackPlugin({
+      title: 'Caching',
+      inject: true,
+      /* minify: true, */
+      /* hash: true, */
+      template: './src/index.html',
+      filename: path.resolve(__dirname, 'dist', 'index.html')
+    }),
+    new webpack.HashedModuleIdsPlugin()
+  ],
   output: {
-    filename: 'main.js',
-    path: path.resolve(__dirname, 'dist')
+    filename: '[name].[contenthash].js',
+    path: path.resolve(__dirname, 'dist', 'static')
   },
   module: {
     rules: [
@@ -15,12 +29,16 @@ module.exports = {
       }
     ]
   },
-  plugins: [ 
-    new HtmlWebpackPlugin({
-      inject: false,
-      hash: true,
-      template: './src/index.html',
-      filename: 'index.html'
-    })
-  ]
+  optimization: {
+    runtimeChunk: 'single',
+    splitChunks: {
+      cacheGroups: {
+        vendor: {
+          test: /[\\/]node_modules[\\/]/,
+          name: 'vendors',
+          chunks: 'all'
+        }
+      }
+    }
+  }
 };
