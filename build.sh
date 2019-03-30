1660248f80a25d4750459077a38068c3c3d882ad
Chris Arderne <chris@rdrn.me>
2019-03-30 10:11:50 +0100

    Add AWS profile to aws sync command

--- build.sh
+++ build.sh
@@ -9,9 +9,9 @@ cp sitemap.xml dist
 echo https://openelec.surge.sh > dist/CNAME
 
 if [ $1 == stage ]; then
-    aws s3 sync ./dist s3://stage.openelec.me/ --delete
+    aws s3 sync ./dist s3://stage.openelec.me/ --delete --profile serverless
 fi
 
 if [ $1 == prod ]; then
-    aws s3 sync ./dist s3://openelec.me/ --delete
+    aws s3 sync ./dist s3://openelec.me/ --delete --profile serverless
 fi
