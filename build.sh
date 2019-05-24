cada720b8d509e25cc92806dcb42448cdcae6906
Chris Arderne <chris@rdrn.me>
2019-05-24 18:41:55 +0200

    Update README.md and build script

--- build.sh
+++ build.sh
@@ -7,10 +7,13 @@ cp openelec_video.mp4 dist
 cp robots.txt dist
 cp sitemap.xml dist
 
-if [ $1 == stage ]; then
-    aws s3 sync ./dist s3://stage.openelec.me/ --delete --profile serverless
-fi
+url=$(sed -n 's/^'$1': //p' config.yml)
+prof=$(sed -n 's/^profile: //p' config.yml)
 
-if [ $1 == prod ]; then
-    aws s3 sync ./dist s3://openelec.me/ --delete --profile serverless
+if [ $1 == dev ]; then
+    light-server -s dist
+elif [ $1 == stage ]; then
+    aws s3 sync ./dist $url --delete --profile $prof
+elif [ $1 == prod ]; then
+    aws s3 sync ./dist $url --delete --profile $prof
 fi
