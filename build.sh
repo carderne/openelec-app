4aff75ff36231a3a835e15ea179d222863521111
Chris Arderne <chris@rdrn.me>
2020-05-09 13:09:02 +0200

    Update build script

--- build.sh
+++ build.sh
@@ -1,4 +1,3 @@
-#pass either dev, stage or prod as the fist argument
 npm run $1
 
 cp src/icons/favicon-1024.png dist
@@ -10,11 +9,3 @@ cp -r data/ dist/
 
 url=$(sed -n 's/^'$1': //p' config.yml)
 prof=$(sed -n 's/^profile: //p' config.yml)
-
-if [ $1 == dev ]; then
-    light-server -s dist -b localhost
-elif [ $1 == stage ]; then
-    aws s3 sync ./dist $url --delete --profile $prof
-elif [ $1 == prod ]; then
-    aws s3 sync ./dist $url --delete --profile $prof
-fi
